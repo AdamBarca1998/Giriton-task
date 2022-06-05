5cf0939a69041a9cb105322702c3ef3296a4879e
Adam Barca <xbarca04@stud.fit.vutbr.cz>
2022-06-05 20:01:37 +0200

    create multithreading logic for get my Ip

---
+++ src/main/java/com/example/application/model/dto/IpDTO.java
@@ -0,0 +1,15 @@
+package com.example.application.model.dto;
+
+import lombok.Data;
+
+import java.net.InetAddress;
+
+/**
+ * @author : Adam Barča
+ * @created : 5. 6. 2022
+ **/
+@Data
+public class IpDTO {
+
+    private InetAddress ip;
+}
--- src/main/java/com/example/application/views/main/MainView.java
+++ src/main/java/com/example/application/views/main/MainView.java
@@ -1,30 +1,53 @@
 package com.example.application.views.main;
 
+import com.example.application.model.dto.IpDTO;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @PageTitle("Main")
 @Route(value = "")
-public class MainView extends HorizontalLayout {
+public class MainView extends VerticalLayout {
 
-    private TextField name;
-    private Button sayHello;
+    private final RestTemplate restTemplate = new RestTemplate();
+    private final ExecutorService executor = Executors.newFixedThreadPool(5);
 
     public MainView() {
-        name = new TextField("Your name");
-        sayHello = new Button("Say hello");
-        sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
-        });
+        Button ipButton = getIpButton();
 
         setMargin(true);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+        setHorizontalComponentAlignment(Alignment.START, ipButton);
 
-        add(name, sayHello);
+        add(ipButton);
     }
 
+    private Button getIpButton() {
+        Button ipButton = new Button("My IP");
+        AtomicInteger orderIndex = new AtomicInteger();
+
+        ipButton.addClickListener(_e -> {
+            executor.submit(() -> {
+                try {
+                    int orderNumber = orderIndex.getAndIncrement();
+                    int sleepTime = (int) (Math.random() * (10 - 5) + 5);
+                    System.out.printf("%d: %ds\n", orderNumber, sleepTime);
+
+                    Thread.sleep(sleepTime * 1000L);
+
+                    IpDTO ip = restTemplate.getForObject("http://ip.jsontest.com/", IpDTO.class);
+                    System.out.printf("%d: %s\n", orderNumber, ip);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        });
+
+        return ipButton;
+    }
 }
